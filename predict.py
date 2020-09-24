67346f3b293ae783c4fe8e70d526b8f96c5ccb80
Rishabh Srivastava <46201552+iamRishabh07@users.noreply.github.com>
2020-09-24 17:38:23 +0530

    Update predict.py

--- predict.py
+++ predict.py
@@ -19,7 +19,6 @@ def model_fn(model_dir):
     """Load the PyTorch model from the `model_dir` directory."""
     print("Loading model.")
 
-    # First, load the parameters used to create the model.
     model_info = {}
     model_info_path = os.path.join(model_dir, 'model_info.pth')
     with open(model_info_path, 'rb') as f:
@@ -27,7 +26,6 @@ def model_fn(model_dir):
 
     print("model_info: {}".format(model_info))
 
-    # Determine the device and construct the model.
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model = LSTMClassifier(model_info['embedding_dim'], model_info['hidden_dim'], model_info['vocab_size'])
 
@@ -64,11 +62,6 @@ def predict_fn(input_data, model):
     
     if model.word_dict is None:
         raise Exception('Model has not been loaded properly, no word_dict.')
-    
-    # TODO: Process input_data so that it is ready to be sent to our model.
-    #       You should produce two variables:
-    #         data_X   - A sequence of length 500 which represents the converted review
-    #         data_len - The length of the review
 
     words = review_to_words(input_data)
     data_X, data_len = convert_and_pad(model.word_dict, words)
@@ -80,12 +73,7 @@ def predict_fn(input_data, model):
     
     data = torch.from_numpy(data_pack)
     data = data.to(device)
-
-    # Make sure to put the model into evaluation mode
     model.eval()
-
-    # TODO: Compute the result of applying the model to the input data. The variable `result` should
-    #       be a numpy array which contains a single integer which is either 1 or 0
 
     with torch.no_grad():
         output = model.forward(data)
